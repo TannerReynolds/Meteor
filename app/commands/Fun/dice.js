74c215e906e58b9ab9d5566e6f50a821b565413c
C:\0BRA\D3V <hpaulson@smc.wtf>
2020-01-04 17:29:37 -0500

    Create dice.js

---
+++ app/commands/Fun/dice.js
@@ -0,0 +1,10 @@
+module.exports = {
+    command: "dice",
+    description: "roll the die!",
+    syntax: ")>dice",
+    category: "Fun",
+    permission: "sendMessages",
+    botPermission: "sendMessages",
+    execute: async (meteor, bot, msg, args) => {
+        msg.channel.createMessage(`:game_die: You rolled a ${Math.floor(Math.random() * 6) + 1}`)
+    }
